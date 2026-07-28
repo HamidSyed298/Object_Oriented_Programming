15cadad456a6fded36ae3a877a4f63a94532394b
Hamid Habib Syed <122344493+HamidSyed298@users.noreply.github.com>
2026-07-28 10:24:06 -0500

    Refactoring

--- Practice_OOP1.py
+++ Practice_OOP1.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as pyplot
 from matplotlib import style
 from datetime import datetime
-
+import torch
 import sklearn.model_selection
 from sklearn import linear_model
 from sklearn.utils import shuffle
@@ -20,7 +20,7 @@ X = np.array(data.drop([predict],1))
 Y = np.array(data[predict])
 best = 0
 for _ in range(30):
-    x_train, x_test, y_train,  y_test = sklearn.model_selection.train_test_split(X,Y,test_size = 0.1)
+    x_train, x_test,  y_test = sklearn.model_selection.train_test_split(X,Y,test_size = 0.1)
 
     linear = linear_model.LinearRegression()
     linear.fit(x_train,y_train)
